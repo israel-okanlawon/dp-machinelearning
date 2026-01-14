f46504bbe18f5b63aa4e1b20161f846b3f7fdff8
Israel Okanlawon <37417155+israel-okanlawon@users.noreply.github.com>
2026-01-14 02:46:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,8 @@ with st.expander('Data'):
 with st.expander('Data Visualization'):
   # "species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+
+# Data preparation
+with st.sidebar:
+  st.header('Input features')

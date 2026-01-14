032ff6737f16b748909a2dbb1bbf85d4459558d5
Israel Okanlawon <37417155+israel-okanlawon@users.noreply.github.com>
2026-01-14 02:21:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+import pandas as pd
 st.title('🤖 Machine Learning App')
 
 st.info('This is app is built on machine learning model')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/087cb5383af3b09e68f6eeb00dea8819c1340de0/penguins_size.csv')
+

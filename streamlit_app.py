ed366eface9f01fa8cede805e83b614db75e85dc
Israel Okanlawon <37417155+israel-okanlawon@users.noreply.github.com>
2026-01-14 11:08:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,6 @@ with st.sidebar:
   gender = st.selectbox('Gender', ('Male', 'Female'))
   bill_length_mm = st.slider('Bil length (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bil depth (mm)', 13.1, 21.5, 17.2)
-  flipper_length_mm = st.slider('Flipper length (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
   

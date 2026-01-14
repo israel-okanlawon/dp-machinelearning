a92de5c09f0465ab97f751bbced59ed39942fce7
Israel Okanlawon <37417155+israel-okanlawon@users.noreply.github.com>
2026-01-14 11:23:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,6 @@ with st.sidebar:
   data = {'island': island,
          'bill_length_mm': bill_length_mm,
           'bill_depth_mm': bill_depth_mm,
-          'bill_depth_mm': bill_depth_mm,
           'flipper_length_mm': flipper_length_mm,
           'body_mass_g': body_mass_g,
           'gender': gender

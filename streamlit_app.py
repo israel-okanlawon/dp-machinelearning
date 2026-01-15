ceb23704d1a2681b251cbb11d548a126bb91a458
Israel Okanlawon <37417155+israel-okanlawon@users.noreply.github.com>
2026-01-15 22:19:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ with st.sidebar:
           'bill_depth_mm': bill_depth_mm,
           'flipper_length_mm': flipper_length_mm,
           'body_mass_g': body_mass_g,
-          'gender': gender
+          'sex': gender
          } 
   input_df = pd.DataFrame(data, index=[0])
 

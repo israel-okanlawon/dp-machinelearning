fbd5403f6f8683bd9860211823ca0aa021a1602d
Israel Okanlawon <37417155+israel-okanlawon@users.noreply.github.com>
2026-01-20 07:10:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ def target_encode(val):
   return target_mapper[val]
 
 y = y_raw.apply(target_encode)
-y = y.rename(columns={'species': 'new_species'})
+y = y.rename(columns={'species': 'new_species'}, inplace=True)
 y
 # y_compare = pd.concat([y, y_raw], axis=1)
 # y_compare

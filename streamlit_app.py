5a1556e49bdd406cd64cc381d9dcf013d40164d9
Israel Okanlawon <37417155+israel-okanlawon@users.noreply.github.com>
2026-01-20 07:23:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,8 +65,6 @@ def target_encode(val):
   return target_mapper[val]
 
 y = y_raw.apply(target_encode)
-y
-y_raw
 
 with st.expander('Data Preparation'):
   st.write('**Encoded X (input penguin)**')

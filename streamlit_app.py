0427b61919a9e5a8e1d95862c940f0dae063943b
Israel Okanlawon <37417155+israel-okanlawon@users.noreply.github.com>
2026-01-20 06:56:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,9 +57,9 @@ def target_encode(val):
   return target_mapper[val]
 
 y = y_raw.apply(target_encode)
-# y
-y_compare = pd.concat([y, y_raw], axis=1)
-y_compare
+y
+y_raw
+
 
 with st.expander('Input Features'):
   st.write('**Input penguin**')

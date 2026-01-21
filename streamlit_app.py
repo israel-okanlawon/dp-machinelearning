f9c71fa814aaea56515be5a9a4fb3ce5c0b04650
Israel Okanlawon <37417155+israel-okanlawon@users.noreply.github.com>
2026-01-21 10:50:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,14 +109,14 @@ st.dataframe(df_prediction_proba,
                 max_value=1
               ),
                'Chinstrap': st.column_config.ProgressColumn(
-                'Adelie',
+                'Chinstrap',
                 format='%f',
                 width='medium',
                 min_value=0,
                 max_value=1
               ),
                'Gentoo': st.column_config.ProgressColumn(
-                'Adelie',
+                'Gentoo',
                 format='%f',
                 width='medium',
                 min_value=0,
